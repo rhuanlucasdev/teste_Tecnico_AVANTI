c491a5e9f55b56eb33d0c0a015fc268131cc1e7d
Rhuan Lucas <rhuanlucassbs@gmail.com>
2025-09-21 16:59:34 -0300

    update index.php

--- index.php
+++ index.php
@@ -1,21 +1,6 @@
 <?php
 require_once "config/db.php";
 
-echo "Conexão com o banco de dados funcionando!";
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Gerenciamento de Estoque</title>
-</head>
-<body>
-  <h1>HELLO WORLD</h1>
-</body>
-<script>
-    window.location.href = "pages/login.php";
-</script>
-</html>
-
+// Redireciona direto para a página de login
+header("Location: pages/login.php");
+exit;
